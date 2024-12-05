e9414f0522c71f23f254b6ad07ef241d4bc4535c
melord <qahramonmansurov0298@gmail.com>
2024-12-06 01:36:33 +0500

    add com

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,19 +1,17 @@
-import { AppSidebar } from "@/components/ui/app-sidebar";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
+    <div className="h-screen w-screen">
       <div className="absolute top-0 right-0 z-50 flex items-center justify-between  bg-background px-4 py-2">
         <ModeToggle />
       </div>
-      <main className="p-4">
-        <SidebarTrigger />
+
+      {/* <SidebarTrigger className="m-2 p-2 " /> */}
+      <main className="p-4 w-full h-full ">
         <Outlet />
       </main>
-    </SidebarProvider>
+    </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import { PinContainer } from "@/components/ui/3d-pin";
+import { Button } from "@/components/ui/button";
 import { Component } from "@/components/ui/charts";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full w-full flex items-center justify-center">
+      <Button>Button</Button>
+      <PinContainer
+        title="Pin"
+        href="https://github.com"
+        // containerClassName="z-50"
+        children={<h1>Hello</h1>}
+      />
       <Component />
     </div>
   );
